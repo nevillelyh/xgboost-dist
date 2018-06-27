84d32200105a466bff57d51d2b994b678c32a97a
Neville Li <neville@spotify.com>
2018-06-27 09:28:16 -0400

    fix maven and cmake setup

--- build.sh
+++ build.sh
@@ -6,36 +6,18 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/common.sh"
 
 UNAME="$(uname -s)"
-if [ "$UNAME" == "Darwin" ]; then
-  CMAKE_BIN="CMake.app/Contents/bin"
-  OS="osx"
-elif [ "$UNAME" == "Linux" ]; then
-  CMAKE_BIN="bin"
-  OS="linux"
-
+if [ "$UNAME" == "Linux" ]; then
   # hack to make sure OpenMP is available on Linux, useful in distributed
   # systems, e.g. Dataflow
   GOMP_PATH=$(ldconfig -p | grep libgomp.so.1 | awk '{print $4}')
   mkdir -p xgboost/jvm-packages/xgboost4j/src/main/resources/lib
   cp $GOMP_PATH xgboost/jvm-packages/xgboost4j/src/main/resources/lib
-else
-  echo "Unkonwn uname $UNAME"
-  exit 1
-fi
-CMAKE_URL="https://cmake.org/files/v3.10/cmake-3.10.0-$UNAME-x86_64.tar.gz"
-MAVEN_URL="http://www-us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz"
-
-if [ ! $(command -v mvn >/dev/null) ]; then
-  curl -s $MAVEN_URL | tar xzf -
-  export PATH="$(pwd)/$(ls | grep maven)/bin":$PATH
 fi
 
-if [ ! $(command -v cmake >/dev/null) ]; then
-  curl -s $CMAKE_URL | tar xzf -
-  export PATH="$(pwd)/$(ls | grep cmake)/$CMAKE_BIN":$PATH
-fi
+cd xgboost
+./build.sh
 
-cd xgboost/jvm-packages
+cd jvm-packages
 VERSION="$POM_VERSION-$GIT_HASH-$OS-SNAPSHOT"
 mvn versions:set -DnewVersion=$VERSION
 mvn -pl xgboost4j clean package
--- common.sh
+++ common.sh
@@ -20,6 +20,31 @@ check_env SONATYPE_USERNAME SONATYPE_PASSWORD
 export SONATYPE_SNAPSHOT_URL="https://oss.sonatype.org/content/repositories/snapshots"
 export SONATYPE_RELEASES_URL="https://oss.sonatype.org/content/repositories/releases"
 
+UNAME="$(uname -s)"
+CMAKE_URL="https://cmake.org/files/v3.11/cmake-3.11.4-$UNAME-x86_64.tar.gz"
+MAVEN_URL="http://apache.spinellicreations.com/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz"
+
+if [ "$UNAME" == "Darwin" ]; then
+  CMAKE_BIN="CMake.app/Contents/bin"
+  OS="osx"
+elif [ "$UNAME" == "Linux" ]; then
+  CMAKE_BIN="bin"
+  OS="linux"
+else
+  echo "Unkonwn uname $UNAME"
+  exit 1
+fi
+
+if [ ! $(command -v mvn >/dev/null) ]; then
+  curl -s $MAVEN_URL | tar xzf -
+  export PATH="$(pwd)/$(ls | grep maven)/bin":$PATH
+fi
+
+if [ ! $(command -v cmake >/dev/null) ]; then
+  curl -s $CMAKE_URL | tar xzf -
+  export PATH="$(pwd)/$(ls | grep cmake)/$CMAKE_BIN":$PATH
+fi
+
 cd xgboost/jvm-packages
 git reset --hard
 export POM_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:exec | tr -d '\n')
